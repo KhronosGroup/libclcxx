dba2f553aae8d2c4776451d4423004746dee4fc9
Ishfaq Wardag <ishfaq.wardag@arm.com>
2021-06-22 11:51:18 +0100

    Added type trait header file and simple test

---
+++ include/opencl_type_traits.h
@@ -0,0 +1,5 @@
+#pragma OPENCL EXTENSION __cl_clang_function_pointers : enable
+#pragma OPENCL EXTENSION __cl_clang_variadic_functions : enable
+#include <type_traits>
+#pragma OPENCL EXTENSION __cl_clang_function_pointers : disable
+#pragma OPENCL EXTENSION __cl_clang_variadic_functions : disable
